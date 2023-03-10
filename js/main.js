eda19a57c2447553babacf551099a1c6f8378403
Unrike <vladik-sto@ya.ru>
2023-03-10 17:29:33 +0300

    ok1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-let randint = function (mix, max){
-  if (max < min) { return 0; };
+const randint = function (min, max){
+  if (max < min) { return 0; }
   return Math.floor(Math.random * (max - min + 1) + min);
-}
+};
 
-let permittedString = function (str, permLen){
-  if (str.length > permLen) { return false; };
+const permittedString = function (str, permLen){
+  if (str.length > permLen) { return false; }
   return true;
-}
+};
