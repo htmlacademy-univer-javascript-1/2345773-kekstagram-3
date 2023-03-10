dee1dbee76b12b463e6825a1497bd6aa75dcd8df
Unrike <vladik-sto@ya.ru>
2023-03-10 22:35:38 +0300

    ok2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ const randint = function (min, max){
   return Math.floor(Math.random * (max - min + 1) + min);
 };
 
+randint(20, 30);
+
 const permittedString = function (str, permLen){
   if (str.length > permLen) { return false; }
   return true;
 };
+
+permittedString("Seven", 7);
