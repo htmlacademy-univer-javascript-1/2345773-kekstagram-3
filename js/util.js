36adf520ac2e48a7ebdf596b01e26d135e823324
Unrike <vladik-sto@ya.ru>
2023-03-13 03:32:12 +0300

    ok

---
+++ js/util.js
@@ -0,0 +1,15 @@
+const randint = function (min, max){
+  if (max < min) { return 0; }
+  return Math.floor(Math.random * (max - min + 1) + min);
+};
+
+
+const permittedString = function (str, permLen){
+  if (str.length > permLen) { return false; }
+  return true;
+};
+
+export{
+  randint,
+  permittedString
+}
